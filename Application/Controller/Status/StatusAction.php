44a32de85ab7ac2677dc8696451d18c5e25082a6
Peter Johnson <wiz@wiz.co.nz>
2011-05-11 01:03:04 +0100

    Added debug information & backtrace to ping.

--- Application/Controller/Status/StatusAction.php
+++ Application/Controller/Status/StatusAction.php
@@ -8,10 +8,23 @@ class StatusAction extends StatusController
 {
     public function action()
     {
+        $backtrace = array();
+        $cleanPath = realpath( $_SERVER[ 'DOCUMENT_ROOT' ] . '/../' ) . '/';
+        foreach( \array_reverse( \debug_backtrace( false ) ) as $k => $trace )
+        {
+            $backtrace[] = ($k+1) . '. ' . $trace[ 'class' ] . '::' . $trace[ 'function' ] . '()';
+        }
+
         $this->request->getHeader( 'Load' );
         $this->response->merge( array(
             'meta'    => $this->request->getHeaders(),
-            'body'    => $this->request->toArray()
+            'body'    => $this->request->toArray(),
+            'debug'=> array(
+                'includes' => \count( \get_included_files() ),
+                'classes' => \count( \get_declared_classes() ),
+                'memory' => floor( \memory_get_peak_usage( ) /1024 ) . 'kb',
+                'trace' => $backtrace
+            )
         ) );
     }
 }
