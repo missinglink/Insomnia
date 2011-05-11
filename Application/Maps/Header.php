5309acfe5e66504b3e831e8f468e2e47bff0f9dd
Peter Johnson <wiz@wiz.co.nz>
2011-05-11 01:05:19 +0100

    Updated response meta section to emulate http response headers

--- Application/Maps/Header.php
+++ Application/Maps/Header.php
@@ -9,11 +9,10 @@ class Header implements Map
     public static function render( $request, $response )
     {
         $header = array();
-        $header[ 'Code' ]           = (int) substr( $response->getCode(), 0, 3 );
-        $header[ 'Content-Type' ]   = $response->getContentType();
-        $header[ 'Character-Set' ]  = $response->getCharacterSet();
+        $header[ 'Code' ]               = (int) \substr( $response->getCode(), 0, 3 );
+        $header[ 'Content-Type' ]       = $response->getContentType() . '; charset=' . $response->getCharacterSet();
 
-        if( $request[ 'version' ] )
+        if( isset( $request[ 'version' ] ) )
             $header[ 'Version' ] = $request[ 'version' ];
 
         $response->prepend( 'meta', $header );
