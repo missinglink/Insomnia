50c8c6d9fb98342eb776187d34502c98ffb94cfa
Peter Johnson <wiz@wiz.co.nz>
2011-05-12 01:06:02 +0100

    Cleanup Application Bootstrap

--- Application/Bootstrap/Insomnia.php
+++ Application/Bootstrap/Insomnia.php
@@ -11,6 +11,18 @@ class Insomnia
 {
     public function __construct()
     {
+        if( \APPLICATION_ENV === 'development' )
+        {
+            \error_reporting( \E_ALL | \E_STRICT );
+            \ini_set( 'display_errors', 'On' );
+        }
+
+        \date_default_timezone_set( 'GMT' );
+        \ini_set( 'session.auto_start',         0 ); // Turn off sessions by default
+        \ini_set( 'session.use_cookies',        0 ); // Turn off cookies by default
+        \ini_set( 'session.use_only_cookies',   0 ); // Turn off cookies by default
+        \ini_set( 'session.use_trans_sid',      0 ); // Turn off sid by default
+
         $classLoader = new ClassLoader( 'Insomnia', '../lib' );
         $classLoader->register();
 
@@ -20,21 +32,12 @@ class Insomnia
         $classLoader = new ClassLoader( 'Symfony', '../lib' );
         $classLoader->register();
 
-        \error_reporting( \APPLICATION_ENV === 'development' ? \E_ALL | \E_STRICT : 0 );
-        \ini_set( 'display_errors', \APPLICATION_ENV === 'development' ? 'On' : 'Off' );
-
-        \date_default_timezone_set( 'GMT' );
-
-        \ini_set( 'session.auto_start',       0 ); // Turn off sessions by default
-        \ini_set( 'session.use_cookies',      0 ); // Turn off cookies by default
-        \ini_set( 'session.use_only_cookies', 0 ); // Turn off cookies by default
-        \ini_set( 'session.use_trans_sid',    0 ); // Turn off sid by default
-
-        \Insomnia\Registry::set( 'request',                 new Request );
-        \Insomnia\Registry::set( 'dispatcher',              new Dispatcher );
-        \Insomnia\Registry::set( 'controller_namespace',    'Application\Controller\\' );
-        \Insomnia\Registry::set( 'action_suffix',           'Action' );
-        \Insomnia\Registry::set( 'view_path',               realpath( dirname( __DIR__ ) . \DIRECTORY_SEPARATOR . 'View' ) . \DIRECTORY_SEPARATOR );
-        \Insomnia\Registry::set( 'layout_path',             realpath( dirname( __DIR__ ) . \DIRECTORY_SEPARATOR . 'View' ) . \DIRECTORY_SEPARATOR );
+        Registry::set( 'request',               new Request );
+        Registry::set( 'dispatcher',            new Dispatcher );
+        Registry::set( 'controller_namespace',  'Application\Controller\\' );
+        Registry::set( 'action_suffix',         'Action' );
+        Registry::set( 'view_path',             \realpath( dirname( __DIR__ ) . \DIRECTORY_SEPARATOR . 'View' ) . \DIRECTORY_SEPARATOR );
+        Registry::set( 'layout_path',           \realpath( dirname( __DIR__ ) . \DIRECTORY_SEPARATOR . 'View' ) . \DIRECTORY_SEPARATOR );
+        Registry::set( 'view_extension',        '.php' );
     }
 }
