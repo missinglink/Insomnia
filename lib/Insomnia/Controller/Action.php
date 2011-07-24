ea917b0b28aab9b1154066ed74d5945f2a4e6de5
Peter Johnson <wiz@wiz.co.nz>
2011-07-24 18:48:49 +0100

    Refactor

--- lib/Insomnia/Controller/Action.php
+++ lib/Insomnia/Controller/Action.php
@@ -18,37 +18,7 @@ abstract class Action
     {
         $validator = new RequestValidator;       
         $this->setValidator( $validator );
-        
         $this->setResponse( new Response );
-        $this->validate();
-    }
-    
-    public function validate()
-    {
-        $reader = new \Insomnia\Router\AnnotationReader( \get_class( $this ) );
-        $params = new \Insomnia\Annotation\Parser\ParamParser(
-                $reader->getMethodAnnotations(
-                        Registry::get( 'dispatcher' )->getRoute()->getReflectionMethod()
-                )
-        );
-        
-        foreach( $params as $param )
-        {
-            switch( $param[ 'type' ] )
-            {
-                case 'integer' : $validator = new \Insomnia\Request\Validator\IntegerValidator( 1, 10 ); break;
-                case 'string' : $validator = new \Insomnia\Request\Validator\StringValidator( 1, 10 ); break;
-            }
-            
-            if( !isset( $validator ) ) throw new Exception( 'Unsupported Request Type "' . $param[ 'type' ] . '"' );
-
-            if( isset( $param[ 'optional' ] ) )
-                $this->validator->optional( $param[ 'name' ], $validator );
-            else
-                $this->validator->required( $param[ 'name' ], $validator );
-        }
-        
-        $this->validator->validate();
     }
 
     public function getResponse()
