4c0968520d23713347dc29687e3334a711d5a14f
Peter Johnson <wiz@wiz.co.nz>
2011-09-25 14:49:38 +0100

    Updated demo opp

--- Application/View/entity/allentities.php
+++ Application/View/entity/allentities.php
@@ -1,15 +1,8 @@
 <?php $this->setTitle( 'Html Entities - Character Entity Reference' ); ?>
+<?php $this->addStylesheet( '/css/entity.css' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=Delius' ); ?>
 
-<style type="text/css">
-    body {
-        background-color: none;
-        background-image: url( 'http://peter.john.so/n/bg.jpg' );
-        background-repeat: repeat-x;
-    }
-</style>
-
-<div style="padding:50px;">
+<div id="outer">
     
     <h1 style="font-family: 'Delius', sans-serif;">Character Entity Reference</h1>
     <br />
@@ -17,27 +10,25 @@
     
     <?php foreach( $this->data as $data ): ?>
 
-        <div style="float:left; width:4em; text-align:center; margin-right:10px;">
+        <div id="entity-list">
 
-            <div style="width:4em; height:4em; border:dotted 1px #ddd; margin:auto; margin-bottom:5px; text-align: center; background-color:#fff;">
+            <div class="entity">
                 <?php if( isset( $data[ 'character' ] ) ): ?>
-                    <p style="font-size:3em; line-height:1.35em;">
-                        <a href="<?php echo $data[ 'uri' ]; ?>.html" style="text-decoration:none; color: #000;"><?php echo $data[ 'character' ]; ?></a>
+                    <p class="font">
+                        <a href="<?php echo $data[ 'uri' ]; ?>.html"><?php echo $data[ 'character' ]; ?></a>
                     </p>
                 <?php endif; ?>
             </div>
 
             <?php if( isset( $data[ 'name' ] ) ): ?>
-            <p style="clear:both;">
-                <a href="<?php echo $data[ 'uri' ]; ?>.html" style="text-decoration:none;"><?php echo $data[ 'name' ]; ?></a>
+            <p class="info">
+                <a href="<?php echo $data[ 'uri' ]; ?>.html"><?php echo $data[ 'name' ]; ?></a>
             </p>
             <?php endif; ?>
 
         </div>
     
     <?php endforeach; ?>
-    
-    <div style="clear:both;">&nbsp;</div>
     
     <hr />
     
--- Application/View/entity/entity.php
+++ Application/View/entity/entity.php
@@ -1,4 +1,5 @@
 <?php $this->setTitle( 'Html Entity - ' . ucfirst( $this->data[ 'description' ] ) ); ?>
+<?php $this->addStylesheet( '/css/entity.css' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=Delius' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=Montez' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=Leckerli+One' ); ?>
@@ -29,23 +30,15 @@
     
 ); ?>
 
-<style type="text/css">
-    body {
-        background-color: none;
-        background-image: url( 'http://peter.john.so/n/bg.jpg' );
-        background-repeat: repeat-x;
-    }
-</style>
-
-<div style="padding:50px;">
+<div id="outer">
     
-    <div style="width:8em; height:8em; border:solid 1px #ddd; padding:0px; margin-bottom:25px; text-align: center; float:left; background-color: #fff;">
+    <div id="entity-large">
         <?php if( isset( $this->data[ 'character' ] ) ): ?>
-            <p style="font-size:7em; line-height:1.15em;"><?php echo $this->data[ 'character' ]; ?></p>
+            <p><?php echo $this->data[ 'character' ]; ?></p>
         <?php endif; ?>
     </div>
     
-    <div style="display:block; float:left; padding:5px 0px 0px 30px;">
+    <div id="entity-info">
         <?php if( isset( $this->data[ 'name' ] ) ): ?>
             <h1 class="error" style="font-family: 'Delius', sans-serif;"><?php echo ucfirst( $this->data[ 'name' ] ); ?></h1>
         <?php endif; ?>
@@ -64,97 +57,97 @@
     </div>
     
 
-    <hr style="margin-bottom:5px;" />
+    <hr style="margin-bottom:0px;" />
     
-        <p style="margin-bottom:0px;">
-            <a href="/" style="text-decoration:none;">Character Entity Reference</a>
+        <p class="breadcrumbs">
+            <a href="/">Character Entity Reference</a>
             &raquo;
             <?php echo ucfirst( $this->data[ 'name' ] ); ?>
         </p>
 
-    <hr style="margin-top:5px;" />
+    <hr  />
 
-    <table style="width:100%">
+    <table id="entity-properties">
 
         <tr>
         <?php if( isset( $this->data[ 'name' ] ) ): ?>
             <th width="300">Entity Name</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'name' ]; ?></td>
+            <td><?php echo $this->data[ 'name' ]; ?></td>
         <?php endif; ?>
         </tr>
         
         <tr>
         <?php if( isset( $this->data[ 'character' ] ) ): ?>
             <th>Character</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'character' ]; ?></td>
+            <td><?php echo $this->data[ 'character' ]; ?></td>
         <?php endif; ?>
         </tr>
         
         <tr>
         <?php if( isset( $this->data[ 'htmlentity' ] ) ): ?>
             <th>HTML Entity</th>
-            <td style=" background-color:#fff;"><?php echo htmlentities( $this->data[ 'htmlentity' ] ); ?></td>
+            <td><?php echo htmlentities( $this->data[ 'htmlentity' ] ); ?></td>
         <?php endif; ?>
         </tr>
         
         <tr>
         <?php if( isset( $this->data[ 'ascii' ] ) ): ?>
             <th>Ascii</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'ascii' ]; ?></td>
+            <td><?php echo $this->data[ 'ascii' ]; ?></td>
         <?php endif; ?>
         </tr>
         
         <tr>
         <?php if( isset( $this->data[ 'unicode' ] ) ): ?>
             <th>Unicode code point (decimal)</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'unicode' ]; ?></td>
+            <td><?php echo $this->data[ 'unicode' ]; ?></td>
         <?php endif; ?>
         </tr>
         <tr>
         <?php if( isset( $this->data[ 'standard' ] ) ): ?>
             <th>Standard</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'standard' ]; ?></td>
+            <td><?php echo $this->data[ 'standard' ]; ?></td>
         <?php endif; ?>
         </tr>
         <tr>
         <?php if( isset( $this->data[ 'dtd' ] ) ): ?>
             <th>DTD</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'dtd' ]; ?></td>
+            <td><?php echo $this->data[ 'dtd' ]; ?></td>
         <?php endif; ?>
         </tr>
         <tr>
         <?php if( isset( $this->data[ 'subset' ] ) ): ?>
             <th>Old ISO subset</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'subset' ]; ?></td>
+            <td><?php echo $this->data[ 'subset' ]; ?></td>
         <?php endif; ?>
         </tr>
         <tr>
         <?php if( isset( $this->data[ 'description' ] ) ): ?>
             <th>Description</th>
-            <td style=" background-color:#fff;"><?php echo $this->data[ 'description' ]; ?></td>
+            <td><?php echo $this->data[ 'description' ]; ?></td>
         <?php endif; ?>
         </tr>
 
         <tr>
         <?php if( isset( $this->data[ 'htmlentity' ] ) ): ?>
             <th>Example</th>
-            <td style=" background-color:#fff;"><?php echo htmlentities( '<p>' . $this->data[ 'htmlentity' ] . '</p>' ); ?></td>
+            <td><?php echo htmlentities( '<p>' . $this->data[ 'htmlentity' ] . '</p>' ); ?></td>
         <?php endif; ?>
         </tr>
 
     </table>
    
-    <hr style="clear:both;" />
+    <hr />
     
     <?php if( isset( $this->data[ 'character' ] ) ): ?>
         <?php foreach( $fonts as $font ): ?>
-            <div style="width:4em; height:4em; padding:0px; margin-bottom:25px; margin-right:5px; text-align: center; float:left; background-color: #fff;">
-                <p style="font-size:3em; line-height:1.3em; font-family: '<?= $font; ?>', sans-serif;"><?php echo $this->data[ 'character' ]; ?></p>
+            <div class="font-example">
+                <p style="font-family: '<?= $font; ?>', sans-serif;"><?php echo $this->data[ 'character' ]; ?></p>
             </div>
         <?php endforeach; ?>
     <?php endif; ?>
     
-    <hr style="clear:both;" />
+    <hr />
     
     <p>This content is available in the following formats:
     <a href="<?php echo $this->data[ 'uri' ]; ?>.xml">xml</a>
