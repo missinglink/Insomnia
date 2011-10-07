67e6521cadf03642db08ede51626a7f93aa051fc
Peter Johnson <fox_manbxc@rcpt.at>
2011-10-07 16:51:35 +0200

    Added Content-Type and DocType headers courtesy of Rowan Manning

--- Application/View/entity/layout.php
+++ Application/View/entity/layout.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title><?= $this->getTitle(); ?></title><?php
             $this->printStylesheetsAsHtml();
             $this->printScriptsAsHtml();
