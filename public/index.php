247d40aad0903456b2b7196b6dfb9560ed6ebd6b
Peter Johnson <insomnia@rcpt.at>
2011-11-16 02:09:19 +0000

    Moved autoloader out of index so it can be used by Phpunit as a bootstrap

--- public/index.php
+++ public/index.php
@@ -2,18 +2,10 @@
 
 namespace Insomnia;
 
-use \Doctrine\Common\ClassLoader;
+require_once 'autoloader.php';
 
-define( 'ROOT', \dirname( \dirname( __FILE__ ) ) . \DIRECTORY_SEPARATOR );
-define( 'APPLICATION_ENV', 'development' );
-
-require_once \ROOT.'/lib/Doctrine/Common/ClassLoader.php';
-spl_autoload_register( array( new ClassLoader( 'Insomnia', \ROOT . 'lib' ), 'loadClass' ) );
-spl_autoload_register( array( new ClassLoader( 'Doctrine', \ROOT . 'lib' ), 'loadClass' ) );
-spl_autoload_register( array( new ClassLoader( 'Application', \ROOT ), 'loadClass' ) );
-spl_autoload_register( array( new ClassLoader( 'Community', \ROOT ), 'loadClass' ) );
-
-$bootstrap = new \Application\Bootstrap\Insomnia;
+// Run Bootstraps
+new \Application\Bootstrap\Insomnia;
 
 Kernel::getInstance()
     
