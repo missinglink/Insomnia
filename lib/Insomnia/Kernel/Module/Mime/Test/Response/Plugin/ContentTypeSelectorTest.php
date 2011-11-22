0ec510561ac51e75c7697b2c1999df0ee92c9c7d
adlawson <adlawson@gmail.com>
2011-11-22 01:09:31 +0000

    Added further content type tests

--- lib/Insomnia/Kernel/Module/Mime/Test/Response/Plugin/ContentTypeSelectorTest.php
+++ lib/Insomnia/Kernel/Module/Mime/Test/Response/Plugin/ContentTypeSelectorTest.php
@@ -2,13 +2,14 @@
 
 namespace Insomnia\Kernel\Module\Mime\Response\Plugin;
 
+use \Insomnia\Registry;
 use \Insomnia\Request;
 use \Insomnia\Response;
 
 /**
  * This test covers all functionality of the ContentTypeSelector plugin
  */
-class ParamParserTest extends \PHPUnit_Framework_TestCase
+class ContentTypeSelectorTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * Configure the test
@@ -16,27 +17,19 @@ class ParamParserTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         // Create test objects
-        $this->response = new Request;
+        $this->request = new Request;
+        Registry::set( 'request', $this->request );
+        
         $this->response = new Response;
         $this->contentPlugin = new ContentTypeSelector;
     }
     
     /**
-     * Test the default mime type is set if none is requested
-     */
-    public function testDefault()
-    {
-        $this->contentPlugin->update($this->response);
-        
-        $this->assertEquals( ContentTypeSelector::TYPE_JSON, $this->response->getContentType(), 'Default mime type should be JSON' );
-    }
-    
-    /**
-     * Request content types
+     * Map requested file extension to response content type
      *
      * @return array 
      */
-    public static function responseMimeDataprovider()
+    public static function responseMimeExtensionDataprovider()
     {
         return array(
             array( '.xml',  'application/xml' ),
@@ -49,23 +42,109 @@ class ParamParserTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
-     * Test that the ParamParser plugin adds all data from the $_REQUEST superglobal
-     * in to the Insomnia request object.
-     * 
-     * @dataProvider responseMimeDataprovider
+     * Map requested header to reponse content type
+     *
+     * @return array 
      */
-    public function testMimeFromExtensionGlobal( $extension, $expectedContent )
+    public static function responseMimeHeaderDataprovider()
+    {
+        return array(
+            array( 'text/xml',   'application/xml' ),
+            array( 'text/yaml' , 'application/x-yaml'),
+            array( 'application/xhtml', 'text/html' )
+        );
+    }
+    
+    /**
+     * Test the default mime type is set if none is requested
+     */
+    public function testDefault()
+    {
+        $this->contentPlugin->update( $this->response );
+        
+        $this->assertEquals( ContentTypeSelector::TYPE_JSON, $this->response->getContentType(), 'Default mime type should be JSON' );
+    }
+    
+    /**
+     * Test that the ContentTypeSelector plugin detects and returns
+     * the requested content type from extension
+     * 
+     * @dataProvider responseMimeExtensionDataprovider
+     */
+    public function testMimeFromExtension( $extension, $expectedContent )
     {
         $this->assertEquals( null, $this->request->getContentType(), 'Content type should be empty' );
         
-        // Set a request path
+        // Set the request path
         // Need a nicer way to define this...
-        $this->request->setParam('path', 'path/to/endpoint' . $extension, $expectedContent);
+        $this->request->setParam( 'path', 'path/to/endpoint' . $extension );
         
         // Run the plugin
         $this->contentPlugin->update( $this->response );
         
-        $this->assertEquals( $expectedContent, $this->request->getContentType(),
-            'Unexpected content type "' . $this->request->getContentType() . '" received for extension "' . $type . '"' );
+        $this->assertEquals( $expectedContent, $this->response->getContentType(),
+            'Unexpected content type "' . $this->response->getContentType() . '" received for extension "' . $extension . '"' );
+    }
+    
+    /**
+     * Test that the ContentTypeSelector plugin detects an
+     * invalid content type from extension
+     * 
+     * Current behavior defines that a default mime is used,
+     * but it would preferably throw an exception
+     */
+    public function testInvalidMimeFromExtension()
+    {
+        $extension = '.invalid';
+        
+        // Set the request path
+        $this->request->setParam( 'path', 'path/to/endpoint' . $extension );
+        
+        // Run the plugin
+        $this->contentPlugin->update( $this->response );
+        
+        $this->assertEquals( 'application/json', $this->response->getContentType(),
+            'Unexpected content type "' . $this->response->getContentType() . '" received for extension "' . $extension . '"' );
+    }
+    
+    /**
+     * Test that the ContentTypeSelector plugin detects and returns
+     * the requested content type from headers
+     * 
+     * @dataProvider responseMimeHeaderDataprovider
+     */
+    public function testMimeFromHeader( $requestedContent, $expectedContent )
+    {
+        $this->assertEquals( null, $this->request->getContentType(), 'Content type should be empty' );
+        
+        // Set the request content header
+        $this->request->setHeader( 'Accept', $requestedContent );
+        
+        // Run the plugin
+        $this->contentPlugin->update( $this->response );
+        
+        $this->assertEquals( $expectedContent, $this->response->getContentType(),
+            'Unexpected content type "' . $this->response->getContentType() . '" received for requested type "' . $requestedContent . '"' );
+    }
+    
+    /**
+     * Test that the ContentTypeSelector plugin detects an
+     * invalid content type from headers
+     * 
+     * Current behavior defines that a default mime is used,
+     * but it would preferably throw an exception
+     */
+    public function testInvalidMimeFromHeader()
+    {
+        $requestedContent = 'application/zip';
+        
+        // Set a request path
+        $this->request->setHeader( 'Accept', $requestedContent );
+        
+        // Run the plugin
+        $this->contentPlugin->update( $this->response );
+        
+        $this->assertEquals( 'application/json', $this->response->getContentType(),
+            'Unexpected content type "' . $this->response->getContentType() . '" received for requested type "' . $requestedContent . '"' );
     }
 }
