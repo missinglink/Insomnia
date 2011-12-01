fab5d5f9ae4f917fb954d2b53a19749601f3876a
Dan Thorpe <dan@bravenewtalent.com>
2011-12-01 17:51:56 +0000

    Adds simplistic array parameter validation
    Signed-off-by: Dan Thorpe <dan@bravenewtalent.com>

--- Community/Module/RequestValidator/Dispatcher/Plugin/ParamAnnotationValidator.php
+++ Community/Module/RequestValidator/Dispatcher/Plugin/ParamAnnotationValidator.php
@@ -7,7 +7,9 @@ use \Insomnia\Annotation\Parser\ParamParser;
 
 use \Community\Module\RequestValidator\Request\Validator\IntegerValidator,
     \Community\Module\RequestValidator\Request\Validator\StringValidator,
-    \Community\Module\RequestValidator\Request\Validator\RegexValidator;
+    \Community\Module\RequestValidator\Request\Validator\RegexValidator,
+    \Community\Module\RequestValidator\Request\Validator\ArrayValidator;
+    
 
 class ParamAnnotationValidator extends Observer
 {
@@ -18,12 +20,14 @@ class ParamAnnotationValidator extends Observer
         $params = new ParamParser( $endpoint->getMethodAnnotations() );
 
         foreach( $params as $param )
-        {
+        {            
             switch( $param[ 'type' ] )
             {
                 case 'integer' : $validator = new IntegerValidator( 1, 10 ); break;
                 case 'string' : $validator = new StringValidator( 1, 10 ); break;
                 case 'regex' : $validator = new RegexValidator( $param[ 'regex' ] ); break;
+                case 'array[string]' : $validator = new ArrayValidator( 'string' ); break;
+                case 'array[numeric]' : $validator = new ArrayValidator( 'numeric' ); break;                
             }
 
             if( !isset( $validator ) ) throw new \Exception( 'Unsupported Request Type "' . $param[ 'type' ] . '"' );
@@ -34,6 +38,6 @@ class ParamAnnotationValidator extends Observer
                 $endpoint->getController()->getValidator()->required( $param[ 'name' ], $validator );
             
             $endpoint->getController()->getValidator()->validate();
-        }
+        }        
     }
 }
