0186f96d7bf78f37ae1b6c591720dc6d3677c461
Peter Johnson <insomnia@rcpt.at>
2011-12-20 03:31:06 +0000

    CSS fix

--- Community/Module/RestClient/View/client/client.php
+++ Community/Module/RestClient/View/client/client.php
@@ -18,7 +18,6 @@
         <div class="insomnia-title">
             <h1 class="error">Client</h1>
             <h4>REST Client</h4>
-            <p>&nbsp</p>
         </div>
     </div>
     <div class="insomnia-documentation">
