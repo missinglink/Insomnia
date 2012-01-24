71fc9cde69212c45b2d6155279edbdb16a9b735a
Peter Johnson <insomnia@rcpt.at>
2012-01-24 00:16:52 +0000

    Moved documentation css out of main css file

--- lib/Insomnia/Kernel/Module/Mime/View/layout/layout.php
+++ lib/Insomnia/Kernel/Module/Mime/View/layout/layout.php
@@ -1,6 +1,7 @@
 <?php $this->addStylesheet( '/insomnia/css/basic.css' ); ?>
 <?php $this->addStylesheet( '/insomnia/css/layout.css' ); ?>
 <?php $this->addStylesheet( '/insomnia/css/insomnia.css' ); ?>
+<?php $this->addStylesheet( '/insomnia/css/insomnia-documentation.css' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=Droid+Serif' ); ?>
 <?php $this->addStylesheet( 'http://fonts.googleapis.com/css?family=PT+Sans' ); ?>
 <html>
