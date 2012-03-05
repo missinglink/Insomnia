2af70abb96f94ea6abf04aba1aca094db2ea5d3b
Peter Johnson <insomnia@rcpt.at>
2012-03-05 16:14:22 +0000

    Added controller and function to documentation::routes endpoint

--- Community/Module/Documentation/Controller/DocumentationController.php
+++ Community/Module/Documentation/Controller/DocumentationController.php
@@ -126,14 +126,14 @@ class DocumentationController extends Action
                 $a[ 'patternRegex' ] = $route->getpatternRegex();
                 $a[ 'methods' ] = $route->getMethods();
                 
+                $a[ 'controller' ] = $route->getClass();
+                $a[ 'function' ] = $route->getReflectionMethod()->getName();
+                
                 $params = new ParamParser( $reader->getMethodAnnotations( $reflectionMethod ) );
                 $a[ 'params' ] = $params->toArray();
                 
                 if( !isset( $doc[ $category ] ) ) $doc[ $category ] = array();
                 $doc[ $category ][] = $a;
-                
-                $a[ 'controller' ] = $route->getClass();
-                $a[ 'function' ] = $route->getReflectionMethod()->getName();
             }
         }
         
