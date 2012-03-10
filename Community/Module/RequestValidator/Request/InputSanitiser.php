9e5584677babf45b4ea7e4092936b00e3c48a6be
Peter Johnson <insomnia@rcpt.at>
2012-03-10 16:52:02 +0000

    Added basic input sanitization

---
+++ Community/Module/RequestValidator/Request/InputSanitiser.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Community\Module\RequestValidator\Request;
+
+/**
+ * A simple class to prevent HTML injection attacks 
+ */
+class InputSanitiser
+{
+    /**
+     * @var string
+     */
+    private $value = '';
+    
+    /**
+     * @param string $uncleanValue
+     */
+    public function __construct( $uncleanValue )
+    {
+        $this->value = $uncleanValue;
+    }
+    
+    /**
+     * @return InputSanitiser
+     */
+    public function stripTags()
+    {
+        $this->value = strip_tags( $this->value );
+        
+        return $this;
+    }
+    
+    /**
+     * @param string $htmlTag
+     * @return InputSanitiser
+     */
+    public function removeTagBody( $htmlTag )
+    {
+        $this->value = preg_replace( '/<'.preg_quote($htmlTag).'\b[^>]*>(.*?)<\/'.preg_quote($htmlTag).'>/i', '', $this->value );
+        
+        return $this;
+    }
+    
+    /**
+     * @return type
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
