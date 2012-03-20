114d161ebe9c00981d3aebebe8e9c54ce957569d
Peter Johnson <insomnia@rcpt.at>
2012-03-20 19:01:13 +0000

    Updated XmlRenderer to handle recursion

--- lib/Insomnia/Kernel/Module/Mime/Response/Renderer/XmlRenderer.php
+++ lib/Insomnia/Kernel/Module/Mime/Response/Renderer/XmlRenderer.php
@@ -8,49 +8,102 @@ use \Insomnia\Response\ResponseInterface,
 
 class XmlRenderer extends ResponseAbstract implements ResponseInterface
 {
-    private $writer,
-            $indent = '   ';
+    const INDENT_STRING = '   ';
+    const MAX_RECURSION = 50;
+    const ROOT_ELEMENT_NAME = 'response';
+    const NUMERIC_KEY_REPLACEMENT = 'item'; // Replace numeric keys with this string.
+    
+    const DEBUG_NONE = 0;
+    const DEBUG_OUTPUT = 1;
+    const DEBUG_VALIDATE = 2;
+    
+    private $debugLevel = self::DEBUG_NONE;
+    private $writer;
 
     public function render()
     {
         $this->writer = new \XMLWriter;
-
-        $this->writer->openURI( 'php://output' );
+        
+        $this->writer->openMemory();
         $this->writer->startDocument( '1.0', 'UTF-8' );
 
-        $this->writer->setIndent( true );
-        $this->writer->setIndentString( $this->indent );
+            $this->writer->setIndent( true );
+            $this->writer->setIndentString( self::INDENT_STRING );
 
-        $this->writer->startElement( 'response' );
-        //$this->writer->writeAttribute( 'version', '1.0' );
+            $this->writer->startElement( self::ROOT_ELEMENT_NAME );
+            
+                //$this->writer->writeAttribute( 'version', '1.0' );
+                $this->writeXML( $this->getResponse()->toArray() );
 
-        $this->writeXML( $this->getResponse()->toArray() );
-
-        $this->writer->endElement();
+            $this->writer->endElement();
+            
         $this->writer->endDocument();
-        $this->writer->flush();
+        
+        print $this->writer->outputMemory( false );
     }
 
-    private function writeXML( $item )
+    private function writeXML( $item, $level = 0 )
     {
         foreach( $item as $key => $item )
         {
-            if( \is_array( $item ) || \is_object( $item ) )
+            if( is_array( $item ) || is_object( $item ) )
             {
-                $this->writer->startElement( \is_numeric( $key ) ? 'item' : $key );
-                $this->writeXML( $item );
-                $this->writer->endElement();
-                continue;
+                // Prevent recursing too deeply.
+                if( $level < self::MAX_RECURSION )
+                {
+                    $this->writer->startElement( $this->getValidXMLElementName( $key ) );
+                    $this->writeXML( $item, $level++ );
+                    $this->writer->endElement();
+                }
+                
+                else $this->debug( 'max recursion reached' );
             }
 
-            if( \is_scalar( $item ) || \is_null( $item ) )
+            else if( is_scalar( $item ) || is_null( $item ) )
             {
-                $this->writer->startElement( \is_numeric( $key ) ? 'item' : $key );
+                $this->writer->startElement( $this->getValidXMLElementName( $key ) );
                 $this->writer->writeCData( $item );
                 $this->writer->endElement();
-                continue;
             }
+            
+            else $this->debug( 'cannot render object of type '. gettype( $item ) );
+        }
+    }
+    
+    private function getValidXMLElementName( $key )
+    {
+        // XML keys cannot start with a number or contain spaces.
+        $key = ( is_numeric( $key ) ? self::NUMERIC_KEY_REPLACEMENT : strtr( $key, ' ', '_' ) );
+
+        // Validate element name
+        if( $this->getDebugLevel() === self::DEBUG_VALIDATE )
+        {
+            // DOMElement throws and exception if the $key is invalid
+            new \DOMElement( $key );
+        }
+        
+        return $key;
+    }
+    
+    private function debug( $message = 'XML Renderer Failed', $level = 1 )
+    {
+        if( $this->getDebugLevel() >= $level )
+        {
+            $this->writer->startElement( 'error' );
+            $this->writer->writeCData( $message  );
+            $this->writer->endElement();
         }
     }
 
+    /**  @return integer  */
+    public function getDebugLevel()
+    {
+        return (int) $this->debugLevel;
+    }
+
+    /** @param integer $debugLevel */
+    public function setDebugLevel( $debugLevel )
+    {
+        $this->debugLevel = $debugLevel;
+    }
 }
