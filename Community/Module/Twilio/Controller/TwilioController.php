ce77889c576f55eb5d495be342e7b7ec53df4bbc
Peter Johnson <insomnia@rcpt.at>
2012-03-23 16:24:03 +0000

    Twilio Module

--- Community/Module/Twilio/Controller/TwilioController.php
+++ Community/Module/Twilio/Controller/TwilioController.php
@@ -103,7 +103,7 @@ class TwilioController extends Action
         
         header( 'application/xml' );
         echo '<Response>';
-        echo '<Sms>You are now chatting with '.( count( $users ) -1 ).' other people.</Sms>';
+        echo '<Sms>You are now chatting with '.count( $users ).' other people.</Sms>';
         echo '<Sms>Commands: join, leave, who, name [your name], topic [new topic], help.</Sms>';
         echo '</Response>';
         die;
