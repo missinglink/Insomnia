6f5b1c730dc1c4a50609d56dc87f52661cb62d1e
Peter Johnson <insomnia@rcpt.at>
2012-03-23 13:13:33 +0000

    Twilio Module

--- Community/Module/Twilio/Controller/TwilioController.php
+++ Community/Module/Twilio/Controller/TwilioController.php
@@ -16,24 +16,45 @@ class TwilioController extends Action
     /**
      * Twilio module for hack day
      * 
-     * @Insomnia\Annotation\Route("", name="twilio_index")
+     * @Insomnia\Annotation\Route("/call", name="twilio_call")
      * @Insomnia\Annotation\Method("GET")
      * @Insomnia\Annotation\Documentation( category="Twilio" )
      * 
      * @Insomnia\Annotation\Layout( "Insomnia\Kernel\Module\Mime\View\Layout" )
      * 
      */
-    public function action()
+    public function call()
     {
         $this->response[ 'Say' ] = 'Hello World';
         
-        $this->logToDisk();
+        $this->logToDisk( 'call' );
     }
     
-    
-    private function logToDisk()
+    /**
+     * Twilio module for hack day
+     * 
+     * @Insomnia\Annotation\Route("/sms", name="twilio_sms")
+     * @Insomnia\Annotation\Method("GET")
+     * @Insomnia\Annotation\Documentation( category="Twilio" )
+     * 
+     * @Insomnia\Annotation\Layout( "Insomnia\Kernel\Module\Mime\View\Layout" )
+     * 
+     */
+    public function sms()
     {
-        $file = tempnam( '/tmp', 'twilio' );
+        $this->response[ 'Sms' ] = 'Hello World';
+        $this->response[ 'Redirect' ] = 'http://api.twilio.com/sms/welcome';
+        
+        $this->logToDisk( 'sms' );
+    }
+    
+    /**
+     *
+     * @param type $type 
+     */
+    private function logToDisk( $type )
+    {
+        $file = tempnam( '/tmp', 'twilio-' . $type . '-' );
         
         file_put_contents( $file, print_r( $_REQUEST, true ), \FILE_APPEND );
         file_put_contents( $file, print_r( $_SERVER, true ), \FILE_APPEND );
