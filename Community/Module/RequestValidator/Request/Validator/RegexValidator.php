f7088bab5c3f1e3ceaf0743ba026f7b05a5ae5bf
Peter Johnson <insomnia@rcpt.at>
2012-03-26 16:22:23 +0100

    Better support for multi-byte

--- Community/Module/RequestValidator/Request/Validator/RegexValidator.php
+++ Community/Module/RequestValidator/Request/Validator/RegexValidator.php
@@ -16,7 +16,7 @@ class RegexValidator
     
     public function validate( $string, $key )
     {
-        if( !is_string( $string ) || !preg_match( $this->pattern, $string ) )
+        if( !is_string( $string ) || !mb_ereg_match( $this->pattern, $string ) )
             throw new ValidatorException( 'regex' );
     }
 }
