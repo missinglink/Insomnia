42fa0972a1ddab4429bf6e88f78c0a871bd99777
Peter Johnson <insomnia@rcpt.at>
2012-03-26 16:21:57 +0100

    Formatting

--- Community/Module/RequestValidator/Request/Validator/EmailValidator.php
+++ Community/Module/RequestValidator/Request/Validator/EmailValidator.php
@@ -10,6 +10,8 @@ class EmailValidator
     public function validate( $string, $key )
     {
         if( !\filter_var( $string, \FILTER_VALIDATE_EMAIL ) )
+        {
             throw new ValidatorException( 'email' );
+        }
     }
 }
